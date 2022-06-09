010fe35901f95447632350191b06883a3fdf2c1e
glease65 <61334592+glease65@users.noreply.github.com>
2022-06-08 22:02:23 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,20 @@
+// My age is 34
+let myAge = 34;
+
+// The early years is 2 years which are equal to 10.5 human years each.
+let earlyYears = 10.5 * 2;
+
+
+// The variable later years is equal to the result of my age - 2
+var laterYears = myAge - 2;
+
+// Later years is my age -2 multiplied by 4. 
+laterYears *= 4;
+
+// My age in dog years equals the early years + the later years 
+var myAgeInDogYears = earlyYears + laterYears;
+
+// My name is Eric and is written in all lower case.
+var myName = ('Eric'.toLowerCase());
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
